bf48d7cd7f4cdbbf184e5b443a109472b0d333d6
luke miles <luke@lukemil.es>
2022-02-10 13:01:45 -0800

    add ownership lookup

--- src/TombIndex.sol
+++ src/TombIndex.sol
@@ -7,33 +7,49 @@ import "openzeppelin/access/Ownable.sol";
 import "base64/base64.sol";
 import "./RomanNumerals.sol";
 
+interface IERC721OwnerOf {
+    function ownerOf(uint256 _tokenId) external view returns (address);
+}
+
 contract TombIndex is ERC721, Ownable, RomanNumeralSubset {
     string public imageURI;
     bool public isFrozen;
+    
+    // Solidity's string type doesn't support emdashes; this is the unicode
+    // value for an embash
+    int private constant emdash = 0xe28093;
 
-    event tombUpdated(uint256 id);
-    enum House { GENESIS, LUX, RONIN, DEVASTATORS, COMETS, SHADOW, TERRA, X2 }
-    mapping(House => string) public Houses;
+    event TombUpdated(uint256 id);
+    enum House { GENESIS, LUX, X2, SHADOW, COMETS, DEVASTATORS, TERRA, RONIN }
+    mapping(House => string) private houses;
     function initHouses() private {
-        Houses[House.GENESIS] = "GENESIS";
-        Houses[House.LUX] = "LUX";
-        Houses[House.RONIN] = "RONIN";
-        Houses[House.DEVASTATORS] = "DEVASTATORS";
-        Houses[House.COMETS] = "COMETS";
-        Houses[House.SHADOW] = "SHADOW";
-        Houses[House.TERRA] = "TERRA";
-        Houses[House.X2] = "X2";
+        houses[House.GENESIS] = "GENESIS";
+        houses[House.LUX] = "LUX";
+        houses[House.X2] = "X2";
+        houses[House.SHADOW] = "SHADOW";
+        houses[House.COMETS] = "COMETS";
+        houses[House.DEVASTATORS] = "DEVASTATORS";
+        houses[House.TERRA] = "TERRA";
+        houses[House.RONIN] = "RONIN";
+    }
+
+    struct deployment {
+        bool deployed;
+        address _contract;
+        uint256 tokenID;
+        uint8 chainID;
     }
 
     struct Tomb {
+        bool _initialized;
         string name;
         uint256 weight;
+        uint256 numberInHouse;
         House house;
-        address deployedContract;
-        uint256 deployedTokenID;
+        deployment deployment;
     }
 
-    mapping(uint256 => Tomb) public tombs;
+    mapping(uint256 => Tomb) public tombByID;
 
     constructor(
         string memory _imageURI,
@@ -44,13 +60,19 @@ contract TombIndex is ERC721, Ownable, RomanNumeralSubset {
         imageURI = _imageURI;
     }
 
-    function deployRonin(address artistAddress) private {
+    function deployRonin(address artistAddress) internal onlyOwner {
         saveTomb(111, Tomb({
+            _initialized: true,
             name: "TERRAIN",
             weight: 18356125,
+            numberInHouse: 10,
             house: House.RONIN,
-            deployedContract: address(this),
-            deployedTokenID: 111
+            deployment: deployment({
+                _contract: address(this),
+                tokenID: 111,
+                chainID: 0,
+                deployed: true
+            })
         }));
 
         _mint(artistAddress, 111);
@@ -59,8 +81,8 @@ contract TombIndex is ERC721, Ownable, RomanNumeralSubset {
     function saveTomb(uint256 id, Tomb memory tomb) public onlyOwner {
         require(!isFrozen, "Contract is frozen");
         require(id > 0 && id <= 177, "Tomb out of bounds");
-        tombs[id] = tomb;
-        emit tombUpdated(id);
+        tombByID[id] = tomb;
+        emit TombUpdated(id);
     }
 
     function setImageURI(string memory _url) public onlyOwner {
@@ -72,20 +94,38 @@ contract TombIndex is ERC721, Ownable, RomanNumeralSubset {
         isFrozen = true;
     }
 
-    function tombName(uint256 id, Tomb memory tomb) public view returns (string memory) {
-        return string(abi.encodePacked("Tomb ", numeral(id), " - ", tomb.name));
+    function tombName(uint256 id, Tomb memory tomb) private view returns (string memory) {
+        return string(abi.encodePacked("Tomb ", numeral(id), ' ', emdash, ' ', tomb.name));
+    }
+
+    function getTombOwner(uint256 id) public view returns (address) {
+        Tomb memory tomb = tombByID[id];
+        require(tomb._initialized, "Tomb doesn't exist");
+        require(tomb.deployment.chainID == 0, "Can only check ownership value for Ethereum mainnet based Tombs");
+        return IERC721OwnerOf(tomb.deployment._contract).ownerOf(tomb.deployment.tokenID);
+    }
+
+    function makeAttribute(string memory name, string memory value, bool isJSONString) private view returns (string memory) {
+        string memory strDelimiter = '';
+        if (isJSONString) {
+            strDelimiter = '"';
+        }
+
+        return string(abi.encodePacked(
+            '{"trait_type":"', name, '", "value":', strDelimiter, value, strDelimiter, '}'
+        ));
     }
 
     function jsonForTomb(uint256 id) public view returns (bytes memory) {
-        Tomb memory tomb = tombs[id];
-        require(tomb.weight != 0, "Tomb doesn't exist");
+        Tomb memory tomb = tombByID[id];
+        require(tomb._initialized, "Tomb doesn't exist");
         return abi.encodePacked('{"name":"',tombName(id, tomb),
                 '","image":"',
-                imageURI, u256toString(id),
-             '.png", "attributes":[{"trait_type":"House","value":"',
-                Houses[tomb.house], 
-                '"},{"trait_type":"Weight","value":', u256toString(tomb.weight), '}]'
-             , '}');
+                imageURI, u256toString(id), '.png", "attributes":[', 
+                makeAttribute('House', houses[tomb.house], true), ',',
+                makeAttribute('Weight', u256toString(tomb.weight), false), ',',
+                makeAttribute('Number in house', u256toString(tomb.numberInHouse), false),
+              ']}');
     }
 
     function tokenURI(uint256 id) public view override returns (string memory) {
--- src/test/TombIndex.t.sol
+++ src/test/TombIndex.t.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.10;
 
 import "ds-test/test.sol";
+import "solmate/tokens/ERC721.sol";
 import "../TombIndex.sol";
 import "base64/base64.sol";
 
@@ -10,19 +11,66 @@ interface CheatCodes {
   function ffi(string[] calldata) external returns (bytes memory);
 }
 
+contract ExampleRonin is ERC721 {
+    constructor() ERC721("Ronin", "RNIN") {
+    }
+
+    function tokenURI(uint256 id) public view override returns (string memory) {
+        return "";
+    }
+    
+    address OtherAddress = 0xfB843f8c4992EfDb6b42349C35f025ca55742D33;
+
+    function mint2() public {
+        _mint(OtherAddress, 0);
+    }
+
+}
+
 
 contract TombContractTest is DSTest {
     CheatCodes cheats = CheatCodes(HEVM_ADDRESS);
+    ExampleRonin internal ExtNFTContract = ExampleRonin(0x517e643F53EB3622Fd2c3A12C6BFde5E7Bc8D5ca);
     address ArtistAddress = 0x4a61d76ea05A758c1db9C9b5a5ad22f445A38C46;
+    address OtherAddress = 0xfB843f8c4992EfDb6b42349C35f025ca55742D33;
     TombIndex internal TombContract;
 
+    enum House { GENESIS, LUX, X2, SHADOW, COMETS, DEVASTATORS, TERRA, RONIN }
+    struct Tomb {
+        bool _active;
+        string name;
+        uint256 weight;
+        House house;
+        address deployedContract;
+        uint256 deployedTokenID;
+    }
+
     function setUp() public {
-        cheats.prank(ArtistAddress);
+        ExtNFTContract = new ExampleRonin();
+        ExtNFTContract.mint2();
         TombContract = new TombIndex("https://tombseri.es/img/", ArtistAddress);
+        TombContract.saveTomb(21, TombIndex.Tomb({
+            _initialized: true,
+            name: "EQUINOX",
+            weight: 18356125,
+            numberInHouse: 10,
+            house: TombIndex.House.RONIN,
+            deployment: TombIndex.deployment({
+                _contract: address(ExtNFTContract),
+                tokenID: 0,
+                chainID: 0,
+                deployed: true
+            })
+        }));
+        TombContract.transferOwnership(ArtistAddress);
     }
 
     function testOwnerOf() public {
         assertEq(TombContract.ownerOf(111), ArtistAddress);
+    }
+
+    function testExternalOwnerOf() public {
+        assertEq(TombContract.getTombOwner(21), OtherAddress);
     }
 
     function testSetURI() public {
@@ -35,7 +83,7 @@ contract TombContractTest is DSTest {
         TombContract.setImageURI("test");
     }
 
-    function testFailDoesntExit() public {
+    function testFailDoesntExist() public {
         emit log(TombContract.tokenURI(178));
     }
 
