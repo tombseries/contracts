d60efcbda5c165a79e83b1b879adc0fc4eeb94c7
James Geary <36774175+jgeary@users.noreply.github.com>
2022-11-28 15:50:48 -0500

    rename and some improvements

--- src/IndexMarkerV2.sol
+++ src/IndexMarkerV2.sol
@@ -1,22 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-import "openzeppelin-upgradeable/proxy/utils/Initializable.sol";
 import "openzeppelin-upgradeable/access/AccessControlUpgradeable.sol";
+import "openzeppelin-upgradeable/proxy/utils/Initializable.sol";
 import "openzeppelin-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "openzeppelin-upgradeable/token/common/ERC2981Upgradeable.sol";
 import "openzeppelin-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 import "openzeppelin-upgradeable/token/ERC721/extensions/ERC721VotesUpgradeable.sol";
 import "openzeppelin-upgradeable/token/ERC721/extensions/IERC721MetadataUpgradeable.sol";
 import "openzeppelin-upgradeable/utils/cryptography/EIP712Upgradeable.sol";
-// TODO: import below from zora recovery repo
 import "./IRecoveryChildV1.sol";
 
-contract WrappedIndexMarker is
+contract IndexMarkerV2 is
   Initializable,
   ERC721Upgradeable,
   AccessControlUpgradeable,
   EIP712Upgradeable,
   ERC721VotesUpgradeable,
+  ERC2981Upgradeable,
   UUPSUpgradeable
 {
   bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
@@ -24,17 +25,24 @@ contract WrappedIndexMarker is
     keccak256("SET_TOMB_VOTING_WEIGHT_ROLE");
   bytes32 public constant SET_TOMB_CONTRACTS_ROLE =
     keccak256("SET_TOMB_CONTRACTS_ROLE");
+  bytes32 public constant SET_ROYALTIES_ROLE = keccak256("SET_ROYALTIES_ROLE");
 
+  uint16 internal constant DEFAULT_TOMB_HOLDER_VOTING_WEIGHT = 30; // 30 votes
+  uint96 internal constant DEFAULT_ROYALTY_BPS = 1_000; // 10%
+  address payable internal constant TOMB_ARTIST =
+    payable(0x4a61d76ea05A758c1db9C9b5a5ad22f445A38C46);
+
+  uint96 public royaltyBps;
   IERC721MetadataUpgradeable public indexMarker;
   mapping(address => bool) public isTombContract;
-  address public recoveryRegistry;
+  mapping(address => mapping(uint256 => bool)) public isSingletonTombToken;
   uint16 public tombHolderVotingWeight;
 
   constructor() {
     _disableInitializers();
   }
 
-  function initialize(address _indexMarker, address _recoveryRegistry)
+  function initialize(address _indexMarker, address payable _royaltyDestination)
     public
     initializer
   {
@@ -43,15 +51,18 @@ contract WrappedIndexMarker is
     __EIP712_init("Wrapped Tomb Index Marker", "1");
     __ERC721Votes_init();
     __UUPSUpgradeable_init();
-
-    _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-    _grantRole(UPGRADER_ROLE, msg.sender);
-    _grantRole(SET_TOMB_VOTING_WEIGHT_ROLE, msg.sender);
-    _grantRole(SET_TOMB_CONTRACTS_ROLE, msg.sender);
+    __ERC2981_init();
 
     indexMarker = IERC721MetadataUpgradeable(_indexMarker);
-    recoveryRegistry = _recoveryRegistry;
-    tombHolderVotingWeight = 1;
+    tombHolderVotingWeight = DEFAULT_TOMB_HOLDER_VOTING_WEIGHT; // equiv to 30 index markers
+    royaltyBps = DEFAULT_ROYALTY_BPS; // 1,000 bps = 10%
+
+    // uncomment below if we allow tokenId 0 to mint
+    /* isSingletonTombToken[_indexMarker][0] = true;
+    _setTokenRoyalty(0, TOMB_ARTIST, royaltyBps); */
+
+    _setDefaultRoyalty(_royaltyDestination, royaltyBps);
+    _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
   }
 
   function tokenURI(uint256 tokenId)
@@ -63,22 +74,15 @@ contract WrappedIndexMarker is
     return indexMarker.tokenURI(tokenId);
   }
 
-  function wrap(uint256 tokenId) public {
+  function mint(uint256 tokenId) public {
+    // remove requirement below to allow tokenId 0 to mint
     require(
       tokenId != 0,
       "WrappedIndexMarker: tokenId 0 is not an Index Marker"
     );
+
     indexMarker.transferFrom(msg.sender, address(this), tokenId);
     _mint(msg.sender, tokenId);
-  }
-
-  function unwrap(uint256 tokenId) public {
-    require(
-      ownerOf(tokenId) == msg.sender,
-      "WrappedIndexMarker: caller is not owner"
-    );
-    _burn(tokenId);
-    indexMarker.transferFrom(address(this), msg.sender, tokenId);
   }
 
   function _getVotingUnits(address account)
@@ -96,7 +100,7 @@ contract WrappedIndexMarker is
         msg.sender
       ).getRecoveryParentToken();
       if (
-        isTombContract[parentToken] &&
+        isTomb(parentToken, parentTokenId) &&
         IERC721Upgradeable(parentToken).ownerOf(parentTokenId) == account
       ) {
         return tombHolderVotingWeight + balanceOf(account);
@@ -118,6 +122,31 @@ contract WrappedIndexMarker is
     }
   }
 
+  function setTombTokens(
+    address[] memory _contracts,
+    uint256[] memory _tokenIds,
+    bool[] memory _isTombToken
+  ) public onlyRole(SET_TOMB_CONTRACTS_ROLE) {
+    require(
+      _contracts.length == _tokenIds.length &&
+        _tokenIds.length == _isTombToken.length,
+      "WrappedIndexMarker: invalid input"
+    );
+    for (uint256 i = 0; i < _contracts.length; i++) {
+      isSingletonTombToken[_contracts[i]][_tokenIds[i]] = _isTombToken[i];
+    }
+  }
+
+  function isTomb(address _tokenContract, uint256 _tokenId)
+    public
+    view
+    returns (bool)
+  {
+    return
+      isTombContract[_tokenContract] ||
+      isSingletonTombToken[_tokenContract][_tokenId];
+  }
+
   function setTombHolderVotingWeight(uint16 _tombHolderVotingWeight)
     public
     onlyRole(SET_TOMB_VOTING_WEIGHT_ROLE)
@@ -125,10 +154,36 @@ contract WrappedIndexMarker is
     tombHolderVotingWeight = _tombHolderVotingWeight;
   }
 
+  function setDefaultRoyalty(address receiver, uint96 feeNumerator)
+    public
+    onlyRole(SET_ROYALTIES_ROLE)
+  {
+    _setDefaultRoyalty(receiver, feeNumerator);
+  }
+
+  function deleteDefaultRoyalty() public onlyRole(SET_ROYALTIES_ROLE) {
+    _deleteDefaultRoyalty();
+  }
+
+  function setTokenRoyalty(
+    uint256 tokenId,
+    address receiver,
+    uint96 feeNumerator
+  ) public onlyRole(SET_ROYALTIES_ROLE) {
+    _setTokenRoyalty(tokenId, receiver, feeNumerator);
+  }
+
+  function resetTokenRoyalty(uint256 tokenId)
+    public
+    onlyRole(SET_ROYALTIES_ROLE)
+  {
+    _resetTokenRoyalty(tokenId);
+  }
+
   function supportsInterface(bytes4 interfaceId)
     public
     view
-    override(ERC721Upgradeable, AccessControlUpgradeable)
+    override(ERC721Upgradeable, AccessControlUpgradeable, ERC2981Upgradeable)
     returns (bool)
   {
     return super.supportsInterface(interfaceId);
