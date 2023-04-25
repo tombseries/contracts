e2a42b2314be0affcc8cf323ffe81acc3d26e63b
luke miles <luke@lukemil.es>
2023-04-24 18:38:31 -0700

    Recovery Governor: add vote aggregator support (#7)
    * Recovery Governor: add vote aggregator support
    
    * Update TombRecoveryGovernor.sol
    
    * Update TombRecoveryGovernor.sol
    
    * vote aggregator
    
    * add tests
    
    * fix tests
    
    * snapshot
    
    ---------
    
    Co-authored-by: jgeary <36774175+jgeary@users.noreply.github.com>

--- src/scripts/DeployRecoveryGovernor.s.sol
+++ src/scripts/DeployRecoveryGovernor.s.sol
@@ -7,22 +7,25 @@ import "../recovery/TombRecoveryGovernor.sol";
 contract DeployRecoveryGovernor is Script {
     address public constant INDEX_MARKER_MAINNET = 0xa5c93e5d9eb8fb1B40228bb93fD40990913dB523;
     address constant INDEX_MARKER_GOERLI = 0x17DB883ed31582A82c69FeEe0B28ac662c877f00;
+    address constant VOTE_AGGREGATOR_MAINNET = address(0);
+    address constant VOTE_AGGREGATOR_GOERLI = address(0);
 
     function run() public {
         if (block.chainid == 1) {
-            run(INDEX_MARKER_MAINNET);
+            run(INDEX_MARKER_MAINNET, VOTE_AGGREGATOR_MAINNET);
         } else if (block.chainid == 5) {
-            run(INDEX_MARKER_GOERLI);
+            run(INDEX_MARKER_GOERLI, VOTE_AGGREGATOR_GOERLI);
         } else {
             revert("Unsupported chain");
         }
     }
 
-    function run(address indexMarker) public {
+    function run(address indexMarker, address voteAggregator) public {
         uint256 key = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(key);
+        require(voteAggregator != address(0), "Vote aggregator is required");
 
-        new TombRecoveryGovernor(indexMarker);
+        new TombRecoveryGovernor(indexMarker, voteAggregator);
 
         vm.stopBroadcast();
     }
